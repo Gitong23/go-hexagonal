9a528bf27146bec5788c6df8789cfc9a9fc34831
gitong23 <gitong23@gmail.com>
2025-01-02 19:51:17 +0700

    refactor errors

--- service/customer_service.go
+++ service/customer_service.go
@@ -1,8 +1,10 @@
 package service
 
 import (
+	"bank/errs"
 	"bank/logs"
 	"bank/repository"
+	"database/sql"
 )
 
 type customerService struct {
@@ -18,7 +20,7 @@ func (s customerService) GetCustomers() ([]CustomerResponse, error) {
 	customers, err := s.custRepo.GetAll()
 	if err != nil {
 		logs.Error(err)
-		return nil, err
+		return nil, errs.NewUnexpectedError()
 	}
 
 	custResponses := []CustomerResponse{}
@@ -38,12 +40,12 @@ func (s customerService) GetCustomer(id int) (*CustomerResponse, error) {
 	customer, err := s.custRepo.GetById(id)
 	if err != nil {
 
-		// if err == sql.ErrNoRows {
-		// 	return nil, errors.New("customer not found")
-		// }
+		if err == sql.ErrNoRows {
+			return nil, errs.NewNotFoundError("customer not found")
+		}
 
 		logs.Error(err)
-		return nil, err
+		return nil, errs.NewUnexpectedError()
 	}
 	// return nil, nil
 
